2346907ad9fbe1bce892a098750b40b5b3863341
LukaSaric <saricluka76@gmail.com>
2026-03-30 22:24:16 +0200

    feat: VBC-50 add Users table Added initial settings for h2 db and hibernate.

---
+++ vibecheck/src/main/java/hr/tvz/vibecheck/entity/User.java
@@ -0,0 +1,32 @@
+package hr.tvz.vibecheck.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long idUser;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private LocalDateTime tstamp;
+
+}
