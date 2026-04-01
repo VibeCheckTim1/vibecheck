678c78e17b62fe814d34051bc2b0f2369f5ed390
LukaSaric <saricluka76@gmail.com>
2026-04-01 18:35:41 +0200

    feat: add /api ony each endpoint Easy /api on each endpoint

---
+++ vibecheck/src/main/java/hr/tvz/vibecheck/config/WebConfig.java
@@ -0,0 +1,15 @@
+package hr.tvz.vibecheck.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forBasePackage("hr.tvz.vibecheck.controller"));
+    }
+}
