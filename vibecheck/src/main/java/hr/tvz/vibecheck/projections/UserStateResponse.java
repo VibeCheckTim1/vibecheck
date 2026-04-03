eec9c4f0a8a60bd7e085362f5ccea18881cbf511
LukaSaric <saricluka76@gmail.com>
2026-04-03 22:14:55 +0200

    feat: more UserState data

--- vibecheck/src/main/java/hr/tvz/vibecheck/projections/UserStateResponse.java
+++ vibecheck/src/main/java/hr/tvz/vibecheck/projections/UserStateResponse.java
@@ -1,10 +1,17 @@
 package hr.tvz.vibecheck.projections;
 
+import hr.tvz.vibecheck.enums.ProfileVisibility;
+
 import java.time.LocalDateTime;
 
 public interface UserStateResponse {
     Long getIdUser();
+    String getFirstName();
+    String getLastName();
     String getUsername();
+    String getAvatarUrl();
+    String getBio();
+    ProfileVisibility getVisibility();
     String getEmail();
     LocalDateTime getTstamp();
 }
