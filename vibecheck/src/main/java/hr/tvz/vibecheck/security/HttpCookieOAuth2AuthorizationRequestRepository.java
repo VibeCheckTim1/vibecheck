12f1f9ca6a42b16707f13e13a9431ed62c4c4c60
LukaSaric <saricluka76@gmail.com>
2026-04-10 19:27:29 +0200

    feat: add oauth2 login/register
    # Conflicts:
    #	vibecheck/src/main/java/hr/tvz/vibecheck/security/SecurityConfig.java

---
+++ vibecheck/src/main/java/hr/tvz/vibecheck/security/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -0,0 +1,69 @@
+package hr.tvz.vibecheck.security;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.util.SerializationUtils;
+import org.springframework.web.util.WebUtils;
+
+import java.util.Base64;
+
+public class HttpCookieOAuth2AuthorizationRequestRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
+
+    public static final String OAUTH2_AUTH_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+    private static final int COOKIE_EXPIRE_SECONDS = 180;
+
+    @Override
+    public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
+        var cookie = WebUtils.getCookie(request, OAUTH2_AUTH_REQUEST_COOKIE_NAME);
+        if (cookie == null) return null;
+
+        return deserialize(cookie);
+    }
+
+    @Override
+    public void saveAuthorizationRequest(OAuth2AuthorizationRequest authRequest,
+                                         HttpServletRequest request,
+                                         HttpServletResponse response) {
+
+        if (authRequest == null) {
+            deleteCookie(request, response);
+            return;
+        }
+
+        var value = serialize(authRequest);
+
+        var cookie = new Cookie(OAUTH2_AUTH_REQUEST_COOKIE_NAME, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(COOKIE_EXPIRE_SECONDS);
+
+        response.addCookie(cookie);
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
+        var authRequest = loadAuthorizationRequest(request);
+        deleteCookie(request, response);
+        return authRequest;
+    }
+
+    private void deleteCookie(HttpServletRequest request, HttpServletResponse response) {
+        var cookie = new Cookie(OAUTH2_AUTH_REQUEST_COOKIE_NAME, null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+
+    private String serialize(OAuth2AuthorizationRequest object) {
+        return Base64.getUrlEncoder().encodeToString(SerializationUtils.serialize(object));
+    }
+
+    private OAuth2AuthorizationRequest deserialize(Cookie cookie) {
+        return (OAuth2AuthorizationRequest) SerializationUtils.deserialize(
+                Base64.getUrlDecoder().decode(cookie.getValue())
+        );
+    }
+}
--- vibecheck/src/main/java/hr/tvz/vibecheck/security/VibeCheckUserDetails.java
+++ vibecheck/src/main/java/hr/tvz/vibecheck/security/VibeCheckUserDetails.java
@@ -8,21 +8,34 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class VibeCheckUserDetails implements UserDetails {
+public class VibeCheckUserDetails implements UserDetails, OAuth2User {
 
     private Long id;
     private String username;
     private String email;
     private String password;
     private List<String> roles;
+    private Map<String, Object> attributes;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public @Nonnull String getName() {
+        return username;
+    }
 
     @Override
     public @Nonnull Collection<? extends GrantedAuthority> getAuthorities() {
