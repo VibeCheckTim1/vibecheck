1db28b2075ff8422c0eace8730aebe3f7a62af2c
Luka Šarić <saricluka76@gmail.com>
2026-04-13 17:57:43 +0200

    feat: error handler poc with SID

---
+++ vibecheck/src/main/java/hr/tvz/vibecheck/security/MDCfilter.java
@@ -0,0 +1,30 @@
+package hr.tvz.vibecheck.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class MDCfilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain chain) throws ServletException, IOException {
+        MDC.put("sid", UUID.randomUUID().toString());
+
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            MDC.remove("sid");
+        }
+    }
+}
