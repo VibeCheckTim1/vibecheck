ea4fed9bdf3fbba245a510a2fc004792b0e747f7
Marko7755 <mgradiscaj@gmail.com>
2026-04-16 16:56:09 +0200

    feature: add error handling for duplicate follow requests

--- vibecheck/src/main/java/hr/tvz/vibecheck/exception/GlobalExceptionHandler.java
+++ vibecheck/src/main/java/hr/tvz/vibecheck/exception/GlobalExceptionHandler.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
 @ControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
@@ -19,12 +22,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateFollowRequestException.class)
     public ResponseEntity<?> handleDuplicateFollowRequestException(DuplicateFollowRequestException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("message", ex.getMessage()));
     }
 
     @ExceptionHandler(DuplicateFollowException.class)
     public ResponseEntity<?> handleDuplicateFollowRequestException(DuplicateFollowException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("message", ex.getMessage()));
     }
 
 }
