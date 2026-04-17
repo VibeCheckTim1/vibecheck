3f92c401d394775cb863b4374ba6d7ee9101e4b9
LukaSaric <saricluka76@gmail.com>
2026-04-17 17:01:12 +0200

    feat: add OAuth2 authentication failure handler and update security config

---
+++ vibecheck/src/main/java/hr/tvz/vibecheck/security/oauth/OAuth2AuthenticationFailureHandler.java
@@ -0,0 +1,31 @@
+package hr.tvz.vibecheck.security.oauth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private final HttpCookieOAuth2AuthorizationRequestRepository authorizationRequestRepository;
+
+    @Value("${app.frontend.base-url}")
+    private String frontendBaseUrl;
+
+    @Override
+    public void onAuthenticationFailure(@NonNull HttpServletRequest request,
+                                        @NonNull HttpServletResponse response,
+                                        @NonNull AuthenticationException exception) throws IOException {
+        authorizationRequestRepository.removeAuthorizationRequest(request, response);
+        getRedirectStrategy().sendRedirect(request, response, frontendBaseUrl + "/login");
+    }
+}
