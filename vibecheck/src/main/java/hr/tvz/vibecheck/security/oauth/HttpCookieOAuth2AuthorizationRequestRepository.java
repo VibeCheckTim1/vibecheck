97b85e020151bd2f28526efcabc36571f6603ae7
LukaSaric <saricluka76@gmail.com>
2026-05-13 20:23:48 +0200

    feat: add configurable cookie security

--- vibecheck/src/main/java/hr/tvz/vibecheck/security/oauth/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ vibecheck/src/main/java/hr/tvz/vibecheck/security/oauth/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -3,6 +3,7 @@ package hr.tvz.vibecheck.security.oauth;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.util.SerializationUtils;
@@ -14,6 +15,9 @@ public class HttpCookieOAuth2AuthorizationRequestRepository implements Authoriza
 
     public static final String OAUTH2_AUTH_REQUEST_COOKIE_NAME = "oauth2_auth_request";
     private static final int COOKIE_EXPIRE_SECONDS = 180;
+
+    @Value("${app.cookies.secure}")
+    private boolean secureCookie;
 
     @Override
     public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
@@ -29,13 +33,14 @@ public class HttpCookieOAuth2AuthorizationRequestRepository implements Authoriza
                                          HttpServletResponse response) {
 
         if (authRequest == null) {
-            deleteCookie(request, response);
+            deleteCookie(response);
             return;
         }
 
         var value = serialize(authRequest);
 
         var cookie = new Cookie(OAUTH2_AUTH_REQUEST_COOKIE_NAME, value);
+        cookie.setSecure(secureCookie);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
         cookie.setMaxAge(COOKIE_EXPIRE_SECONDS);
@@ -46,13 +51,15 @@ public class HttpCookieOAuth2AuthorizationRequestRepository implements Authoriza
     @Override
     public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
         var authRequest = loadAuthorizationRequest(request);
-        deleteCookie(request, response);
+        deleteCookie(response);
         return authRequest;
     }
 
-    private void deleteCookie(HttpServletRequest request, HttpServletResponse response) {
+    private void deleteCookie(HttpServletResponse response) {
         var cookie = new Cookie(OAUTH2_AUTH_REQUEST_COOKIE_NAME, null);
+        cookie.setSecure(secureCookie);
         cookie.setPath("/");
+        cookie.setHttpOnly(true);
         cookie.setMaxAge(0);
         response.addCookie(cookie);
     }
@@ -61,6 +68,7 @@ public class HttpCookieOAuth2AuthorizationRequestRepository implements Authoriza
         return Base64.getUrlEncoder().encodeToString(SerializationUtils.serialize(object));
     }
 
+    @SuppressWarnings("java:S1874")
     private OAuth2AuthorizationRequest deserialize(Cookie cookie) {
         return (OAuth2AuthorizationRequest) SerializationUtils.deserialize(
                 Base64.getUrlDecoder().decode(cookie.getValue())
